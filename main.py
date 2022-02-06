b322a04fb89b878d716c687eebca470e3e1c9463
Alya <chepurova@bk.ru>
2022-02-07 00:09:24 +0300

    implemented new algorithm

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from mrl98 import *
+from streaming import Stream
+import numpy as np
 
-b = 10
-k = 5
+
+b = 50
+k = 12
+n = 5000000
+q = 0.1
 buffers = [Buffer(k, 0) for i in range(b)]
 
 for i in range(b):
     new_operation(buffers[i], [0, 1, 2, 4, 7])
 
-print(collapse_operation([buffers[0], buffers[1]]))
+# print(buffers)
+# new_buffers = [buffers[i] for i in range(b) if i % 2 == 0]
+# collapse_operation(new_buffers)
+# print(buffers)
 # print(buffers[0])
 
-# print(buffers[1])
+# # print(buffers[1])
+
+stream = Stream(k, n)
+print(munro_paterson(b, k, q, stream))
+
+stream = Stream(k, n)
+print(mrl98_new(b, k, q, stream))
--- mrl98.py
+++ mrl98.py
@@ -1,19 +1,23 @@
 import numpy as np
-from math import ceil
+from math import ceil, floor
 from streaming import Stream
 
 
 class Buffer:
-    def __init__(self, k, w, label=False, seq=[]):
+    def __init__(self, k, w, label=False, seq=[], level=None):
         self.k = k
         self.weight = w
         # whether empty or full, False in case of empty
         self.label = label
         self.seq = seq
+        self.level = level
 
     def __repr__(self):
         contain = "Full" if self.label else "Empty"
-        return "{} buffer with k={}, weight={}, seq={}".format(contain, self.k, self.weight, self.seq)
+        repr_str = "{} buffer with k={}, weight={}, seq={}".format(contain, self.k, self.weight, self.seq)
+        if self.level is not None:
+            repr_str += ", level={}".format(self.level)
+        return repr_str
 
 
 def new_operation(b: Buffer, seq: list):
@@ -35,20 +39,21 @@ def collapse_operation(buffers: list[Buffer]):
     bucket.sort()
     # print("sorted bucket ", bucket)
 
-    offset = int((w + 1) / 2)
+    offset = (w + 1) / 2 if w % 2 == 1 else w / 2
     # print("offset: ", offset)
     new_seq = []
     for i in range(k):
-        position = i*w + offset + (w % 2) * (i % 2)
+        # position = i*w + int(offset) + (w % 2 == 0) * (i % 2)*2
+        position = i * w + int(offset)
         # print((w % 2) * (i % 2), position)
         new_seq.append(bucket[position])
 
     y = Buffer(k, w, label=True, seq=new_seq)
     # print(y.seq, y.k, y.weight)
 
-    for b in buffers:
-        b.weight, b.label, b.seq = 0, False, []
     buffers[0] = y
+    for b in buffers[1:]:
+        b.weight, b.label, b.seq = 0, False, []
 
 
 def output_operation(buffers: list[Buffer], quantile: float):
@@ -64,17 +69,15 @@ def output_operation(buffers: list[Buffer], quantile: float):
 
 def munro_paterson(b: int, k: int, q: float, stream: Stream) -> int:
     buffers = [Buffer(k, 0, False, []) for i in range(b)]
-    empty = [i for i in range(b)]
-    full = []
     big_seq = []
     try:
         while True:
+            empty = [i for i in range(b) if buffers[i].label is False]
+            full = [i for i in range(b) if buffers[i].label is True]
 
             if len(empty) > 0:
                 seq = next(stream.stream_k_values())
                 new_operation(buffers[empty[0]], seq)
-                full.append(empty[0])
-                empty.pop(0)
                 big_seq += seq
 
             else:
@@ -87,7 +90,6 @@ def munro_paterson(b: int, k: int, q: float, stream: Stream) -> int:
                     else:
                         seen.append(w)
                 collapse_operation([buffers[i1], buffers[i2]])
-                empty.append(i2)
 
     except StopIteration:
         full_buffers = [buffers[i] for i in full]
@@ -96,3 +98,41 @@ def munro_paterson(b: int, k: int, q: float, stream: Stream) -> int:
         print(np.quantile(big_seq, q))
         return output_operation(full_buffers, q)
 
+
+def mrl98_new(b: int, k: int, q: float, stream: Stream) -> int:
+    buffers = [Buffer(k, 0, False, []) for i in range(b)]
+    big_seq = []
+
+    try:
+
+        while True:
+            empty = [i for i in range(b) if buffers[i].label is False]
+            full = [i for i in range(b) if buffers[i].label is True]
+
+            if len(empty) > 1:
+                for i in empty:
+                    seq = next(stream.stream_k_values())
+                    new_operation(buffers[i], seq)
+                    buffers[i].level = 0
+                    big_seq += seq
+
+            elif len(empty) == 1:
+                seq = next(stream.stream_k_values())
+                l = min([buffers[i].level for i in full])
+                new_operation(buffers[empty[0]], seq)
+                buffers[empty[0]].level = l
+                big_seq += seq
+
+            else:
+                l = min([buffers[i].level for i in full])
+                buf_idx_l = [i for i in range(b) if buffers[i].level==l]
+                collapse_operation([buf for buf in buffers if buf.level == l])
+                buffers[buf_idx_l[0]].level = l+1
+
+
+
+    except StopIteration:
+        full_buffers = [buffers[i] for i in range(b) if buffers[i].label is True]
+        big_seq.sort()
+
+        return output_operation(full_buffers, q)
